4ffad856503c2c7d225729a3b072923445ea302a
Alex Severson <alexsvrsn@gmail.com>
2020-09-24 13:36:25 -0500

    aded components for dashboard and pet list items

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+
 class DashboardPage extends Component {
-    render() {
-        return(
-            <p>Dashboard Page</p>
-        );
-    };
+
+  componentDidMount = () => {
+    // fetch pets for dom
+    this.props.dispatch({
+      type: 'FETCH_PETS'
+    })
+  }
+
+  render() {
+    return (
+      // Show Pets here
+      <div>
+        <p>Dashboard Page</p>
+        <table>
+          <tr>
+            <th>Owner</th>
+            <th>Pet</th>
+            <th>Breed</th>
+            <th>Color</th>
+            <th>Checked In</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            {this.props.pets.map(pet => {
+              return (
+                <PetListItems key={pet.id} pet={pet} /> // Component for pets 
+              )
+            })}
+          </tr>
+        </table>
+      </div>
+    );
+  };
 };
 
 const mapStateToProps = (reduxState) => {
