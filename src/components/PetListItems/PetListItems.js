9f5b99e27d9eb9b53100ebfa95a6f36de8004daf
Alex Severson <alexsvrsn@gmail.com>
2020-09-25 09:07:41 -0500

    added style for table and owner

--- src/components/PetListItems/PetListItems.js
+++ src/components/PetListItems/PetListItems.js
@@ -1,32 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class PetListItem extends Component {
-  
+//MATERIAL-UI
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import {
+    Button,
+    TableCell,
+    TableRow,
+} from '@material-ui/core';
 
-  render() {
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
 
-    const pet = this.props.pet
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+
+
+function PetListItem(props) {
+
+
+    
+    const pet = props.pet
 
     return (
-      <div>
-        
-        <td>{pet.owner}</td>
-        <td>{pet.pet_name}</td>
-        <td>{pet.breed}</td>
-        <td>{pet.color}</td>
-        <td>{!pet.checked_in ? "yes" : "no"}</td>
-        <td>
-            <button>Delete</button>
-            {!pet.checked_in ?
-                <button>Check Out</button>
-                :
-                <button>Check In</button>
-            }
-        </td>
-      </div>
+        <>
+            <StyledTableRow component="th" scope="row">
+                <StyledTableCell>{pet.name}</StyledTableCell>
+                <StyledTableCell>{pet.pet_name}</StyledTableCell>
+                <StyledTableCell>{pet.breed}</StyledTableCell>
+                <StyledTableCell>{pet.color}</StyledTableCell>
+                <StyledTableCell align="center">{pet.checked_in ? "yes" : "no"}</StyledTableCell>
+                <StyledTableCell>
+                    <Button
+                        color="secondary"
+                        onClick={props.dispatch({ type: 'DELETE_PET', payload: pet.id })}
+                    >
+                        Delete
+                    </Button>
+                    {pet.checked_in ?
+                        <Button
+                            onClick={props.dispatch({ type: 'CHECK_OUT', payload: pet.id})}
+                        >
+                            Check Out
+                        </Button>
+                        :
+                        <Button
+                            onClick={props.dispatch({ type: 'CHECK_IN', payload: pet.id})}
+                        >
+                            Check In
+                        </Button>
+                    }
+                </StyledTableCell>
+            </StyledTableRow>
+        </>
     );
-  }
+
 }
 
 export default connect()(PetListItem);
